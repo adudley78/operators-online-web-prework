0c26b4b24a24e31661857abfff4e998ef8be2001
Adam Dudley <dudleyadam@gmail.com>
2018-11-19 18:35:13 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,6 +3,7 @@ def unsafe?(speed)
   return boolean
 	end
 def not_safe?(speed)
-  speed > 60 && speed < 40 ? boolean : boolean
+  speed > 60 ? boolean : boolean
+	speed < 40 ? boolean : boolean
   end
 end
